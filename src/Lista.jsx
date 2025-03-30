9a23ca20614dfb5ca4e1ff78656b91160737f02f
Alex <a.johny87@gmail.com>
2025-03-29 23:01:37 -0300

    links-dos-bancos

--- src/Lista.jsx
+++ src/Lista.jsx
@@ -1,15 +1,15 @@
 export function Lista() {
 
 return  <ol>
-            <li>Banco do Brasil</li>
-            <li>Caixa Economica</li>
-            <li>Itau</li>
-            <li>Santander</li>
-            <li>Banco do Nordeste</li>
-            <li>BMG</li>
-            <li>Banco Inter</li>
-            <li>PicPay</li>
-            <li>Mercado Pago</li>
-            <li>Banco Safra</li>
+            <li><a href="https://www.bb.com.br/site/">Banco do Brasil</a></li>
+            <li><a href="https://www.caixa.gov.br/Paginas/home-caixa.aspx">Caixa Economica</a></li>
+            <li><a href="https://www.itau.com.br/">Itau</a></li>
+            <li><a href="https://www.santander.com/en/home">Santander</a></li>
+            <li><a href="https://www.bnb.gov.br/">Banco do Nordeste</a></li>
+            <li><a href="https://www.bancobmg.com.br/">Banco BMG</a></li>
+            <li><a href="https://inter.co/">Banco Inter</a></li>
+            <li><a href="https://picpay.com/">PicPay</a></li>
+            <li><a href="https://www.mercadopago.com.br/">Mercado Pago</a></li>
+            <li><a href="https://banco.bradesco/html/classic/index.shtm">Bradesco</a></li>
         </ol>
 }
