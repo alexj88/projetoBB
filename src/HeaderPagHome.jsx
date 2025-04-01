cab3de7b50fe3a9366577be505091de68f127dd4
alexsandro souza <alexsander83@hotmail.com>
2025-04-01 11:29:44 -0300

    novos componentes

--- src/HeaderPagHome.jsx
+++ src/HeaderPagHome.jsx
@@ -5,8 +5,7 @@ return (
             
           <strong >Bem Vindo ao AcessInvest App</strong>
           <p><a href='#Inicio'>Inicio</a></p>
-          <p><a href='#Sobre'>Sobre</a></p>
-          <p><a href='#Acessibilidade'>Acessibilidade</a></p>
+          
           
           </div>
     </header>
