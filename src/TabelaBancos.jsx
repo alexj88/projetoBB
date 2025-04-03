1451dd9eaf3e55cc76536e1bb6f405a6f6326f9a
alexsandro souza <alexsander83@hotmail.com>
2025-04-03 06:22:03 -0300

    atualização dos links dos investimentos

--- src/TabelaBancos.jsx
+++ src/TabelaBancos.jsx
@@ -1,96 +1,102 @@
+
 export function TabelaBancos() {
 
-return  (
-
-<div>
+    return  (
+    
     <div>
-        <h3>Top 10 Melhores Bancos em Acessibilidade</h3>
+        <div>
+            
+        </div>
+        <div className="table-primary">
+        <table class="table table-sm"  >
+    
+            <thead>
+            <td colspan='4'>Top 10 Melhores Bancos em Acessibilidade</td>
+            </thead>
+            <thead>
+                <th>Banco</th>
+                <th>Ranking</th>
+                <th>Nota</th>
+                <th>Link</th>
+            </thead>
+            
+            <tbody>
+    
+            <tr class="table-primary">
+                <td>Banco do Nordeste</td>
+                <td>1º</td>
+                <td>97</td>
+                <td><a href="https://bnb.gov.br"><img alt="link para o Banco do Nordeste" src="https://cdn.worldvectorlogo.com/logos/banco-do-nordeste.svg" >
+                </img></a></td>
+                </tr>
+    
+                <tr>
+                <td>Banco BMG</td>
+                <td>2º</td>
+                <td>97</td>
+                <td><a href="https://www.bancobmg.com.br/investimentos/"><img alt="link para o Banco BMG" src="https://www.bancobmg.com.br/lumis-theme/br/com/bmg/portal/theme/bmg-portal/img/logo-bmg.svg" ></img></a></td>
+                </tr>
+    
+                <tr class='table-primary'>
+                <td>Caixa Economica Federal</td>
+                <td>3º</td>
+                <td>94</td>
+                <td><a href="https://www.caixa.gov.br/investimentos/Paginas/default.aspx"><img alt="link para Caixa Economica Federal" src="https://cdn.worldvectorlogo.com/logos/caixa-economica-federal-1.svg" ></img></a></td>
+                </tr>
+    
+                <tr>
+                <td>Itaú</td>
+                <td>4º</td>
+                <td>94</td>
+                <td><a href="https://www.itau.com.br/investimentos"><img alt="link para o Banco Itaú" src="https://cdn.worldvectorlogo.com/logos/itau-4.svg" ></img></a></td>
+                </tr>
+    
+                <tr class="table-primary">
+                <td>Mercado Pago</td>
+                <td>5º</td>
+                <td>93</td>
+                <td><a href="https://www.mercadopago.com.br/conta"><img alt="link para o Mercado Pago" src="https://www.dashcontroles.com.br/wp-content/uploads/2023/08/mercado-pago-logo.png" ></img></a></td>
+                </tr>   
+    
+                <tr>
+                <td>Bradesco</td>
+                <td>6º</td>
+                <td>92</td>
+                <td><a href="https://banco.bradesco/html/classic/portal-investimentos/index.shtm"><img alt="link para o Bradesco" src="https://banco.bradesco/assets/classic/img/portal-investimento/logo-bradesco-vermelho.png" ></img></a></td>
+                </tr>
+    
+                <tr class="table-primary">
+                <td>PicPay</td>
+                <td>7º</td>
+                <td>91</td>
+                <td><a href="https://picpay.com/investimentos"><img alt="link para o PicPay" src="https://cdn.worldvectorlogo.com/logos/picpay-1.svg" ></img></a></td>
+                </tr>
+    
+                <tr>
+                <td>Nubank</td>
+                <td>8º</td>
+                <td>84</td>
+                <td><a href="https://www.nubank.com.br/nu/investimentos"><img alt="link para o nubank" src="https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-2.png" ></img></a></td>
+                </tr>
+    
+                <tr class="table-primary">
+                <td>Banco do Brasil</td>
+                <td>9º</td>
+                <td>82</td>
+                <td ><a href="https://www.bb.com.br/site/investimentos/"><img src="https://gfi-portal-web.apps.bb.com.br/appms/assets/img/logo-bb.png" alt="link para o banco do brasil"   ></img></a></td>
+                </tr>
+    
+                <tr>
+                <td>Banco Santander</td>
+                <td>10º</td>   
+                <td>77</td> 
+                <td><a href="https://www.santander.com.br/banco/investimentos"><img alt="link para o Banco do Santander" src="https://cdn.worldvectorlogo.com/logos/santander1.svg" ></img></a></td>
+                </tr>
+               
+            </tbody>
+        </table>
+        </div>
     </div>
-    <div className="TabelaBancos">
-    <table  className="tbBancos" border='1'>
-
-        <thead>
-            <th>Banco</th>
-            <th>Ranking</th>
-            <th>Nota</th>
-            <th>Link</th>
-        </thead>
-        
-        <tbody>
-
-        <tr>
-            <td>Banco do Nordeste</td>
-            <td>1º</td>
-            <td>97</td>
-            <td><a href="https://bnb.gov.br"><img alt="link para o Banco do Nordeste" src="https://play-lh.googleusercontent.com/QdVVwRuK77Fv5ZuoAEb7WSLlPIDVd4D0R3w2Y2nITvRCRYaBGU2zqT9KeGMN9kMBY3A" width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>Banco BMG</td>
-            <td>2º</td>
-            <td>97</td>
-            <td><a href="https://www.bancobmg.com.br/"><img alt="link para o Banco BMG" src="https://images.seeklogo.com/logo-png/1/2/banco-bmg-logo-png_seeklogo-15987.png" width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>Caixa Economica Federal</td>
-            <td>3º</td>
-            <td>94</td>
-            <td><a href="https://www.caixa.gov.br/Paginas/home-caixa.aspx"><img alt="link para Caixa Economica Federal" src="https://cdn.worldvectorlogo.com/logos/caixa-economica-federal-1.svg" width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>Itaú</td>
-            <td>4º</td>
-            <td>94</td>
-            <td><a href="https://www.itau.com.br/"><img alt="link para o Banco Itaú" src="https://cdn.worldvectorlogo.com/logos/itau-4.svg" width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>Mercado Pago</td>
-            <td>5º</td>
-            <td>93</td>
-            <td><a href="https://mercadopago.com.br"><img alt="link para o Mercado Pago" src="https://www.dashcontroles.com.br/wp-content/uploads/2023/08/mercado-pago-logo.png" width={40}></img></a></td>
-            </tr>   
-
-            <tr>
-            <td>Bradesco</td>
-            <td>6º</td>
-            <td>92</td>
-            <td><a href="https://banco.bradesco/html/classic/index.shtm"><img alt="link para o Bradescp" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIPuo6mK940WYxjRWHvUmsDZMQpr8ox9OlIQ&s" width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>PicPay</td>
-            <td>7º</td>
-            <td>91</td>
-            <td><a href="https://picpay.com/"><img alt="link para o PicPay" src="https://d2r9epyceweg5n.cloudfront.net/apps/2233-pt_BR-small-perfi_loja.png" width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>Nubank</td>
-            <td>8º</td>
-            <td>84</td>
-            <td><a href="https://www.nubank.com.br/"><img alt="link para o nubank" src="https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-2.png" width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>Banco do Brasil</td>
-            <td>9º</td>
-            <td>82</td>
-            <td border="0"><a href="https://www.bb.com.br/site/"><img src="https://gfi-portal-web.apps.bb.com.br/appms/assets/img/logo-bb.png" alt="link para o banco do brasil"   width={40}></img></a></td>
-            </tr>
-
-            <tr>
-            <td>Banco Santander</td>
-            <td>10º</td>   
-            <td>77</td> 
-            <td><a href="https://www.santander.com.br"><img alt="link para o Banco do Santander" src="https://cdn.worldvectorlogo.com/logos/santander1.svg" width={40}></img></a></td>
-            </tr>
-           
-        </tbody>
-    </table>
-    </div>
-</div>
-)
-}
+    )
+    }
+    
