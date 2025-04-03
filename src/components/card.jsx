7da93cb46ea210ea0f97c52e39b0a7bc9b07f6d6
alexsandro souza <alexsander83@hotmail.com>
2025-04-03 04:51:08 -0300

    um bocado de coisas

---
+++ src/components/card.jsx
@@ -0,0 +1,18 @@
+import styles from "./Nav.module.css"
+export function Cartao(props){
+    return (
+        <div class="imagemcartao"
+>        <div class="card" >
+             <img src="https://www.toroinvestimentos.com.br/assets-next/images/lp-parceiros-trader/n-assessoria-2x.webp" alt="imagem">
+             </img>
+             <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+             <p class="card-text">Algum conteúdo aqui.</p>
+             <a href="www.toroinvestimentos.com.br" class="btn btn-primary">ir ao site</a>
+                </div>
+        </div>
+    </div>        
+    )
+
+
+}
